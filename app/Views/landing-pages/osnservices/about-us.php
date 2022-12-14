1d54de98113d50c466929c6d3502e5a98d6c096e
Arvind Kumar <arvindkumar2575@gmail.com>
2022-12-15 00:46:55 +0530

    add new htmls

--- app/Views/landing-pages/osnservices/about-us.php
+++ app/Views/landing-pages/osnservices/about-us.php
@@ -6,23 +6,23 @@
 
 <div class="container">
     <p class="lead" style="padding: 40px 0 0 0;text-align: center;">
-        OSN is a team of a 8 members who specialized in their field containing<br>
+        OSN is a team of a 5 members who specialized in their field containing<br>
     </p>
     <p class="lead" style="text-align: center;">
         <b>
             Income Tax Return Filing <br>
+            GST Registration<br>
             Dashboard Design <br>
             Accounting Work <br>
             Website Development <br>
             Form Design (UI &amp; UX) <br>
             Theme Development <br>
             Logo Designing <br>
-            Mobile App Development <br>
         </b><br>
 
     </p>
     <p class="lead" style="text-align: justify;">
-        We provide end-to-end services of all our products. We provide services first and then collect our charges and make reminder calls to our customers on ITR filing. Development of softwares for small business that are developed on customer demands. Customer satisfaction is our main priority so that your money does not die.<br>
+        We provide end-to-end services of all our products. We provide services first and then collect our charges and make reminder calls to our customers on each & every product end date. Development of softwares for small business that are developed on customer demands. Customer satisfaction is our main priority so that your money does not die.<br>
         We respect our customers feedback &amp; trying to resolve as soon as possible because we know <b>Your Time is Precious</b>
     </p>
     <p class="lead" style="text-align: center;">
--- app/Views/landing-pages/osnservices/components/features.php
+++ app/Views/landing-pages/osnservices/components/features.php
@@ -1,10 +1,25 @@
 <div class="alternate-white">
 <div class="container marketing content">
     <div class="row featurette">
-        <div class="col-md-7">
+        <div class="col-md-7 order-md-2">
             <h2 class="featurette-heading fw-normal lh-1">ITR Filing</h2>
-            <p class="lead">Filing your Income Tax Return with OSN Services, give you the best & discount rates from market price. Amazing Cashback and redeem your discount on referral customers.</p>
+            <p class="lead">Filing your Income Tax Return with OSN Services, we give you the best & discount rates from market price. Amazing cashback and redeem your discount on referral friend/others.</p>
             <p>Write your query <a href="<?= base_url('contact-us?q=itr-filing') ?>">here</a> to know more our services.</p>
+        </div>
+        <div class="col-md-5 order-md-1">
+            <img class="tile-img" src="<?= base_url('/assets/images/firstpage-img-3.png') ?>" alt="income-tax-filling" />
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="alternate-white">
+<div class="container marketing content">
+    <div class="row featurette">
+        <div class="col-md-7">
+            <h2 class="featurette-heading fw-normal lh-1">GST Registration</h2>
+            <p class="lead">Register you business on government portal and get your gst number, which separate you from other business and give you unique identity in market.</p>
+            <p>Write your query <a href="<?= base_url('contact-us?q=gst-registration') ?>">here</a> to know more our services.</p>
         </div>
         <div class="col-md-5">
             <img class="tile-img" src="<?= base_url('/assets/images/firstpage-img-3.png') ?>" alt="income-tax-filling" />
@@ -33,7 +48,7 @@
     <div class="row featurette">
         <div class="col-md-7">
             <h2 class="featurette-heading fw-normal lh-1">Accounting Work</h2>
-            <p class="lead">We are here to helps you in your Business by providing our Accounting Tools specially for accounting & MIS related problem.</p>
+            <p class="lead">We help you in your business growth by providing our Accounting Tools specially design for accounting & MIS related tasks.</p>
             <p>Write your query <a href="<?= base_url('contact-us?q=accounting') ?>">here</a> to know more our services.</p>
         </div>
         <div class="col-md-5">
